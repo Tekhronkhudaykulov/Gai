010209d283649458f7269a694c50260827236c78
Tekhronkhudaykulov <tekhronkhudaykulov@gmail.com>
2024-05-01 14:34:03 +0500

    fix

--- src/views/home/view.tsx
+++ src/views/home/view.tsx
@@ -1,24 +1,32 @@
+import Logo from "../../assets/images/img_gai.png";
+
 const Home = () => {
   const date = new Date();
   const showTime = date.getHours() + ":" + date.getMinutes();
 
   return (
     <div className="flex">
-      <div className="h-screen w-[70%]">
-        <div className="flex justify-between px-[20px]  h-[80px] bg-[#F6F6F7] items-center">
-          <p className="text-[35px]">Logo</p>
+      <div className="h-screen w-[70%] !shadow-2xl">
+        <div className="flex justify-between px-[20px]  h-[130px] bg-[#F6F6F7] items-center">
+          <img
+            className="w-[100px] h-[100px] object-contain"
+            src={Logo}
+            alt=""
+          />
           <div>
             <p className="text-[35px] font-bold text-end"> {showTime}</p>
             <p className="text-[20px] font-bold">01.05.2024</p>
           </div>
         </div>
         <div className="p-[20px] flex items-center justify-center text-white bg-[black] h-[100vh]">
-          <div>
-            <p className="text-[35px]">logo</p>
-          </div>
+          <img
+            className="w-[500px] h-[500px] object-contain"
+            src={Logo}
+            alt=""
+          />
         </div>
       </div>
-      <div className="w-[30%] flex flex-col gap-y-[10px]  bg-[#F6F6F7] p-[20px]">
+      <div className="w-[30%] shadow-2xl flex flex-col gap-y-[10px]  bg-[#F6F6F7] p-[20px]">
         <div className="grid  grid-cols-2 rounded-[8px] shadow-inner bg-[green] p-[20px]">
           <div className="flex items-center bg-[white] rounded-[8px] justify-center ">
             <p className="text-[45px]  px-[35px] py-[20px] font-bold">Y49</p>
